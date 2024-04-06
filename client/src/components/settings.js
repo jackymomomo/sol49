faaa861e4569006db25dfb4a234495e3c1ee4e1f
Jack Robinson <jackrobinson@Jacks-MacBook-Air.local>
2024-04-05 19:03:47 -0700

    max kwh a user is willing to sell, persiste users settings upon login

--- client/src/components/settings.js
+++ client/src/components/settings.js
@@ -1,14 +1,17 @@
 import React, { useState, useEffect } from 'react';
-import { db } from '../firebase-config'; 
+import { db } from '../firebase-config';
 import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { useNavigate } from 'react-router-dom'; // Import useNavigate hook
 import NavBar from './navbar';
 
 const Settings = () => {
     const [minPrice, setMinPrice] = useState(0);
     const [maxPrice, setMaxPrice] = useState(0);
+    const [maxKWh, setMaxKWh] = useState(0);
     const auth = getAuth();
     const firestore = getFirestore();
+    const navigate = useNavigate(); // Initialize the useNavigate hook
 
     useEffect(() => {
         const unsubscribe = onAuthStateChanged(auth, user => {
@@ -20,6 +23,7 @@ const Settings = () => {
                         const userData = docSnap.data();
                         setMinPrice(userData.minPrice);
                         setMaxPrice(userData.maxPrice);
+                        setMaxKWh(userData.maxKWh);
                     }
                 });
             }
@@ -30,6 +34,7 @@ const Settings = () => {
 
     const handleMinPriceChange = (e) => setMinPrice(e.target.value);
     const handleMaxPriceChange = (e) => setMaxPrice(e.target.value);
+    const handleMaxKWhChange = (e) => setMaxKWh(e.target.value);
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -38,8 +43,11 @@ const Settings = () => {
             if (user) {
                 const uid = user.uid;
                 const userSettingsRef = doc(firestore, "userSettings", uid);
-                setDoc(userSettingsRef, { minPrice, maxPrice }, { merge: true })
-                    .then(() => console.log("Settings saved successfully"))
+                setDoc(userSettingsRef, { minPrice, maxPrice, maxKWh }, { merge: true })
+                    .then(() => {
+                        console.log("Settings saved successfully");
+                        navigate('/dashboard'); // Navigate to /dashboard after saving
+                    })
                     .catch((error) => console.error("Error saving settings: ", error));
             }
         });
@@ -58,6 +66,11 @@ const Settings = () => {
                 <label>
                     Maximum Price (per kWh):
                     <input type="number" value={maxPrice} onChange={handleMaxPriceChange} />
+                </label>
+                <br />
+                <label>
+                    Maximum kWh to Sell:
+                    <input type="number" value={maxKWh} onChange={handleMaxKWhChange} />
                 </label>
                 <br />
                 <button type="submit">Save</button>
