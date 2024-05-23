dabe4f8c4b5daf3403eea845b4d251236972408b
Jack Robinson <jackrobinson@Jacks-MacBook-Air.local>
2024-05-22 18:37:24 -0700

    speedometer changes colour based on mode user is in fixed some scss

--- client/src/components/speedometer.js
+++ client/src/components/speedometer.js
@@ -1,19 +1,37 @@
 import React from 'react';
 import '../scss/speedometer.scss';
 
-const SpeedometerGauge = ({ currentWatts, maxWatts }) => {
+const SpeedometerGauge = ({ currentWatts, maxWatts, mode }) => {
   const percentage = Math.min(100, (currentWatts / maxWatts) * 100); // Ensure percentage does not exceed 100
+
+  let gaugeColor;
+  let label;
+
+  switch (mode) {
+    case 'buy':
+      gaugeColor = '#00ff00'; // Green for buying
+      label = 'Buying';
+      break;
+    case 'sell':
+      gaugeColor = '#ff0000'; // Red for selling
+      label = 'Selling';
+      break;
+    default:
+      gaugeColor = '#808080'; // Grey for off
+      label = 'Off';
+      break;
+  }
 
   return (
     <div className="gaugeContainer">
       <div className="gaugebox">
         <div className="shadow"></div>
         <div className="content">
-          <div className="percent" data-text="Watts" style={{ '--num': percentage }}>
-            <div className="dot"></div>
+          <div className="percent" data-text={label} style={{ '--num': percentage, '--color': gaugeColor }}>
+            <div className="dot" style={{ backgroundColor: gaugeColor }}></div>
             <svg>
               <circle cx="70" cy="70" r="70"></circle>
-              <circle cx="70" cy="70" r="70"></circle>
+              <circle cx="70" cy="70" r="70" style={{ stroke: gaugeColor }}></circle>
             </svg>
           </div>
           <div className="number">
